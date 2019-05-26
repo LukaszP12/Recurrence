259e9d4664b23b2fd63dd1c64f4d6436d01cdfc4
LukaszP12 <lukaszp4@onet.eu>
2019-05-27 00:47:21 +0200

    1. commit

--- src/Class.java
+++ src/Class.java
@@ -11,10 +11,65 @@ public class Class {
         return fibonacci(n-1) + fibonacci(n-2);
     }
 
+    public static int example(int n){
+        if(n<=0)
+            return 1;
+        else
+            return n*example(n-1);
+    }
+
+    //1 examp
+    public static int sum(int n){
+        if (n == 1)
+            return 1;
+        else
+            return n + sum(n - 1);
+    }
+    //2 examp - powering
+    public static int result(int n) {
+        if (n == 1)
+            return 2;
+        else
+            return 2 * result(n - 1);
+    }
+
+    //Example 3
+    public static int mystery(int n, int a, int d){
+        if (n == 1)
+            return a;
+        else
+            return d + mystery(n - 1, a, d);
+    }
+
+    //Example 4
+    public static int f(int k, int n)
+    {
+        if (n == k)
+            return k;
+        else
+            if (n > k)
+                return f(k, n - k);
+            else
+                return f(k - n, n);
+    }
+
+    // Part of recursion exercises
+    
+
 
     public static void main(String args[]){
         System.out.println(factorial(5));
-        System.out.println(fibonacci(7));
+        System.out.println(fibonacci(5));
+
+        System.out.println(example(4));
+
+        System.out.println(sum(10));
+
+        System.out.println(result(4));
+
+        System.out.println(mystery(3,2,6));
+
+        System.out.println(f(6,8));
     }
 
 }
